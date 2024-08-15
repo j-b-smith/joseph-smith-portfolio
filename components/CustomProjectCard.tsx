eb0cac52d9884909aa2b4d818180dea35226f35a
Joseph Smith <jsmith47265@gmail.com>
2024-08-15 16:41:24 -0400

    Add technology badges to project cards

--- components/CustomProjectCard.tsx
+++ components/CustomProjectCard.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import GoogleAnalytics from "@/utils/GoogleAnalytics";
+
+const badgeImages = {
+  Cypress: "/badges/cypress.png",
+  TypeScript: "/badges/typescript.png",
+  Selenium: "/badges/selenium.png",
+  Playwright: "/badges/playwright.png",
+  PyTest: "/badges/pytest.svg",
+  Python: "/badges/python.png",
+  React: "/badges/react.png",
+  Django: "/badges/django.png",
+  Java: "/badges/java.png",
+  JavaFX: "/badges/javafx.png",
+  MySQL: "/badges/mysql.png",
+  NextJS: "/badges/nextjs.svg",
+  ExpressJS: "/badges/expressjs.png",
+  NodeJS: "/badges/nodejs.png",
+  MongoDB: "/badges/mongodb.png",
+} as const; 
+
+type Technology = keyof typeof badgeImages;
 
 interface CustomProjectCardProps {
   customTitle: string;
   repoName: string;
   summary: string;
   html_url: string;
+  technologies: Technology[];
 }
 
 const CustomProjectCard: React.FC<CustomProjectCardProps> = ({
@@ -14,27 +36,46 @@ const CustomProjectCard: React.FC<CustomProjectCardProps> = ({
   repoName,
   summary,
   html_url,
+  technologies,
 }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col h-full">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col">
       <div
         className="w-full h-16 flex px-4 py-1"
         style={{
           background: "linear-gradient(to right, #667eea, #764ba2)",
         }}
       >
-        <h2 className="text-lg font-bold text-white">{customTitle}</h2>
+        <h2 className="text-lg text-white">{customTitle}</h2>
       </div>
       <div className="p-4 flex flex-col flex-grow">
-        <h3 className="text-md font-semibold text-gray-900 mb-2">{repoName}</h3>
+        <div className="flex flex-row justify-content space-x-3 mb-4">
+          {technologies.map((tech) => (
+            <Image
+              key={tech}
+              src={badgeImages[tech]}
+              alt={`${tech} badge`}
+              width={50} // Set the width
+              height={50} // Set the height
+              className="object-contain"
+            />
+          ))}
+        </div>
+        <h3 className="text-md font-semibold text-gray-900 mb-2">
+          <strong>Repository:</strong> {repoName}
+        </h3>
         <p className="text-sm text-gray-700 mb-4 flex-grow break-words">
-          {summary}
+          <strong>Summary:</strong> {summary}
         </p>
         <Link
           href={`${html_url}/blob/main/README.md`}
           target="_blank"
           rel="noopener noreferrer"
-          onClick={() => GoogleAnalytics.trackLinkClick(`"Check out the code!" clicked for ${repoName}`)}
+          onClick={() =>
+            GoogleAnalytics.trackLinkClick(
+              `"Check out the code!" clicked for ${repoName}`
+            )
+          }
           className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600 transition-colors duration-300 text-center mt-auto"
         >
           Check out the code!
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,45 +7,68 @@ const Projects = () => {
 
   const projectData = [
     {
+      repoName: 'joseph-smith-portfolio',
+      customTitle: 'My Portfolio',
+      summary: 'Here you can view the code behind my portfolio website.',
+      technologies: ['NextJS', 'React', 'TypeScript'],
+    },
+    {
       repoName: 'automation-testing-cookbook',
       customTitle: 'Automation Testing Cookbook',
       summary: 'This is the parent repository for my automation example repositories. (Cypress, Playwright, Selenium etc.)',
+      technologies: ['Cypress', 'TypeScript', 'Playwright', 'Selenium', 'PyTest', 'Python'],
     },
     {
       repoName: 'cypress-typescript-example',
       customTitle: 'Cypress Testing with TypeScript',
       summary: 'A comprehensive example of using Cypress with TypeScript for end-to-end testing of https://magento.softwaretestingboard.com/.',
+      technologies: ['Cypress', 'TypeScript'],
     },
     {
       repoName: 'playwright-typescript-example',
       customTitle: 'Playwright Testing with TypeScript',
       summary: 'An example of using Playwright with TypeScript for end-to-end testing of https://the-internet.herokuapp.com/',
+      technologies: ['Playwright', 'TypeScript'],
     },
     {
       repoName: 'Mario-Party-Scorekeeper',
       customTitle: 'Mario Party Scorekeeper',
       summary: 'My first full-stack application using the MERN stack! I\'ve been a huge fan of Mario Party since I was a kid and this was a lot of fun!',
+      technologies: ['MongoDB', 'ExpressJS', 'React', 'NodeJS'],
     },
     {
       repoName: 'Depot-Utility',
       customTitle: 'Depot Utility',
       summary: 'This is an automation tool I wrote working in IT to submit warranty requests to Dell Tech Direct. It uses Java, JavaFx, and MySQL database.',
+      technologies: ['Java', 'JavaFX', 'MySQL'],
     },
     {
       repoName: 'Inventory-Management-Application',
       customTitle: 'Inventory Management Application',
       summary: 'This was my capstone project for my software engineering course. It Java, JavaFX, and MySQL database.',
+      technologies: ['Java', 'JavaFX', 'MySQL'],
     },
     {
       repoName: 'Software-2-Scheduling-App',
       customTitle: 'Scheduling App',
       summary: 'This was my Software Engineering II project. It uses Java, JavaFX, and MySQL database.',
+      technologies: ['Java', 'JavaFX', 'MySQL'],
     },
-  ];
+  ] as const;
+
+  for (const project of projectData) {
+    type Technology = keyof typeof project['technologies']
+  }
+
+  const sortedRepos = repos.sort((a, b) => {
+    const indexA = projectData.findIndex((project) => project.repoName === a.name);
+    const indexB = projectData.findIndex((project) => project.repoName === b.name);
+    return indexA - indexB;
+  });
 
   return (
-    <div className="container mx-auto py-10 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-      {repos.map((repo) => {
+    <div className="container mx-auto py-10 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {sortedRepos.map((repo) => {
         const project = projectData.find((p) => p.repoName === repo.name);
 
         if (!project) return null;
@@ -57,6 +80,7 @@ const Projects = () => {
             repoName={repo.name}
             summary={project.summary}
             html_url={repo.html_url}
+            technologies={[...project.technologies]}
           />
         );
       })}
