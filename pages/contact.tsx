2b1f169abc2df9313cb3484b685e1a7586c40fe7
Joseph Smith <jsmith47265@gmail.com>
2024-08-16 12:46:15 -0400

    fix covering send message button on mobile

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ import ContactFormCard from '../components/ContactFormCard';
 
 const Contact = () => {
   return (
-    <div className="container mx-auto flex flex-col lg:flex-row gap-8 py-10 px-6" style={{ height: 'calc(100vh - 8rem)' }}>
+    <div className="container mx-auto flex flex-col lg:flex-row gap-8 py-10 px-6 pb-16 min-h-full">
       <ContactInfoCard />
       <ContactFormCard />
     </div>
