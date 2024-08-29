1358eeacd5d650d4916b9077b9fdf535b68eadbb
Joseph Smith <jsmith47265@gmail.com>
2024-08-29 12:56:11 -0400

    update about section

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,45 +4,38 @@ import ImageGallery from "../components/ImageGallery";
 const About = () => {
   return (
     <div className="container flex flex-col lg:flex-row gap-4 my-4 items-stretch pt-8 lg:h-[calc(100vh-7rem)]">
-      <div className="bg-white shadow-lg rounded-lg p-3 flex-1 flex flex-col justify-between">
-        <h1 className="text-2xl font-bold mb-3">About Me 🚀</h1>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 mb-2 leading-tight">
-          Hey there! 👋 I&#39;m Joseph, a Software Engineer with a focus on{" "}
-          <strong>Quality Automation</strong>. My expertise lies in tools like{" "}
-          <strong>Cypress</strong>, <strong>TypeScript</strong>,{" "}
-          <strong>PyTest</strong>, <strong>Python</strong>,{" "}
-          <strong>Selenium</strong>, and <strong>PHPUnit</strong>. I&#39;ve also
-          worked with <strong>Playwright</strong>, <strong>React</strong>,{" "}
-          <strong>Next.js</strong>, and <strong>Django</strong>. 🛠️
+      <div className="bg-white shadow-lg rounded-lg p-4 flex-1 flex flex-col justify-between">
+        <h1 className="text-3xl font-bold mb-4">About Me 🚀</h1>
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-4 leading-snug">
+          Hey there! 👋 I&#39;m Joseph, and when I’m not diving deep into the world
+          of quality automation, you’ll probably find me scaling a rock face, lifting heavy at the gym, or exploring the great outdoors. 
+          I’m passionate about pushing my limits, both in my professional life and in my personal pursuits. 🌟
         </p>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 mb-2 leading-tight">
-          Beyond the code, I&#39;m passionate about music 🎸—I&#39;ve been playing
-          guitar for around 15 years. My favorite genre&#39;s to play are metalcore and acoustic.
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-4 leading-snug">
+          My perfect weekend? It’s all about climbing and camping with my friends, my girlfriend Halle, 
+          and my dog Himi. Whether it’s bouldering, sport climbing, or trad climbing, I’m all in. 🧗‍♂️
         </p>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 mb-2 leading-tight">
-          In my free time, I&#39;m the head routesetter at a local climbing gym 🧗‍♂️,
-          and I&#39;m converting my Promaster van 🚐 into a mobile home for climbing
-          trips with my dog, Himalaya 🐕, and my girlfriend.
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-4 leading-snug">
+          One of my favorite adventures was a 90-hour solo road trip where I camped in the Moab desert. 
+          I got hailed on in my campervan before the sky cleared, revealing a double rainbow. 
+          (You can check out the photo in my gallery!) 🌈🚐
         </p>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 mb-2 leading-tight">
-          Himalaya is the best adventure buddy I could ask for. I also enjoy
-          cooking, powerlifting, weightlifting, and exploring the world of
-          mushrooms 🍄. When I&#39;m not out exploring, I love watching Seinfeld 📺
-          and dreaming about my next travel destination.
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-4 leading-snug">
+          I’ve learned from powerlifting that every rep adds up, just like every small action in our lives. 
+          From climbing, I’ve realized that there are many ways to tackle a problem, and they all have their pros and cons. 💪🧩
         </p>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 mb-2 leading-tight">
-          I&#39;ve traveled to New York, Colorado, Salt Lake City, and Moab, Utah,
-          and I frequently climb at the Red River Gorge in Kentucky. On my list
-          of must-see places: the Redwood National Forest 🌲 and the San Juan
-          Mountains for some mushroom hunting.
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-4 leading-snug">
+          If I had my ultimate adventure, I’d take a road trip down the coastal highway from Washington to see 
+          the California redwoods and Yosemite. 🚐🌲🌉
         </p>
-        <p className="sm:text-sm md:text-lg lg:text-lg text-gray-700 leading-tight">
-          Let&#39;s connect and see where our next adventure takes us! 🚀
+        <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 leading-snug">
+          I’m a fast-paced, excited person with a lot of ideas, always looking for the next challenge. 
+          Let’s connect and see where our next adventure takes us! 🚀🌍
         </p>
       </div>
 
       {/* Image Gallery Card */}
-      <div className="bg-white shadow-lg rounded-lg p-3 flex-1">
+      <div className="bg-white shadow-lg rounded-lg p-4 flex-1">
         <ImageGallery />
       </div>
     </div>
